3c185ef225227770fcf1e770d8f96d7b40a959dc
Santhosh Kumar Manavasi Lakshminarayanan <santhoshkumarml@gmail.com>
2014-11-25 03:53:47 -0500

    Finishing time based graph seperation creation

--- LBP/sample.py
+++ LBP/sample.py
@@ -4,12 +4,16 @@ Created on Nov 25, 2014
 @author: Santhosh Kumar
 '''
 import SIAUtil
-from SIAUtil import review
 import dataReader
+import networkx as nx
 
 if __name__ == '__main__':
     (userIdToUserDict, \
      businessIdToBusinessDict,\
-      reviews) = dataReader.parseAndCreateObjects('E:\\workspace\\\dm\\data\\crawl_old\\o_new_2.txt')
-    graphs = SIAUtil.createTimeBasedGraph(userIdToUserDict, businessIdToBusinessDict, reviews, '1-M')
-    print 'sample', graphs
+      reviews) = dataReader.parseAndCreateObjects('E:\\workspace\\\dm\\data\\crawl_old\\sample_master.txt')
+    cross_day_graphs = SIAUtil.createTimeBasedGraph(userIdToUserDict, businessIdToBusinessDict, reviews, '1-M')
+    cross_month_graphs = SIAUtil.createTimeBasedGraph(userIdToUserDict, businessIdToBusinessDict, reviews, '1-M')
+    cross_year_graphs = SIAUtil.createTimeBasedGraph(userIdToUserDict, businessIdToBusinessDict, reviews, '1-M')
+    print [len(c.nodes) for key in cross_day_graphs.iterkeys() for c in sorted(nx.connected_component_subgraphs(cross_day_graphs[key],False))]
+    print [len(c.nodes) for key in cross_month_graphs.iterkeys() for c in sorted(nx.connected_component_subgraphs(cross_month_graphs[key],False))]
+    print [len(c.nodes) for key in cross_year_graphs.iterkeys() for c in sorted(nx.connected_component_subgraphs(cross_year_graphs[key],False))]
