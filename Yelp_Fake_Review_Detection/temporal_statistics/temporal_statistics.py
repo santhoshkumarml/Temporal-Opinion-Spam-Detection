cf368a84b422bdfacd7e5d11cbbdc60a77c88ceb
Santhosh Kumar Manavasi Lakshminarayanan <Santhosh Kumar Manavasi Lakshminarayanan>
2015-01-02 01:22:04 -0500

    Statistics without Entropy Score and Max Text Similarity

--- Yelp_Fake_Review_Detection/temporal_statistics/temporal_statistics.py
+++ Yelp_Fake_Review_Detection/temporal_statistics/temporal_statistics.py
@@ -3,19 +3,18 @@ Created on Dec 29, 2014
 
 @author: santhosh
 '''
-import util.dataReader as dataReader
-from datetime import datetime
-import sys
-from util import SIAUtil
+from datetime import datetime,timedelta
+import math
 import networkx
 import numpy
-from datetime import date, timedelta
+import random
 import re
+import sys
+
 import matplotlib.pyplot as plt
-from matplotlib.pyplot import legend
-import itertools
-import math
-import pylab
+from util import SIAUtil
+import util.dataReader as dataReader
+
 
 AVERAGE_RATING = 'Average Rating'
 RATING_ENTROPY = 'Rating entropy'
@@ -23,7 +22,8 @@ NO_OF_REVIEWS = 'Number of Reviews'
 RATIO_OF_SINGLETONS = 'Ratio of Singletons'
 RATIO_OF_FIRST_TIMERS = 'Ratio of First-timers'
 YOUTH_SCORE = 'Youth Score'
-ENTROPY_SCORE = 'Entropy SCORE'
+ENTROPY_SCORE = 'Entropy Score'
+RATING_DISTRIBUTION = 'Rating Distribution'
 MAX_TEXT_SIMILARITY = 'Max Text Similarity'
 MEASURES = [AVERAGE_RATING, RATING_ENTROPY, NO_OF_REVIEWS, RATIO_OF_SINGLETONS, RATIO_OF_FIRST_TIMERS, YOUTH_SCORE,\
             ENTROPY_SCORE, MAX_TEXT_SIMILARITY]
@@ -151,6 +151,14 @@ class TemporalGraph(networkx.Graph):
 
 def sigmoid_prime(x):
     return (2.0/(1+math.exp(-x)))-1
+
+def entropyFn(probability_dict):
+    entropy = 0
+    for key in probability_dict:
+        probability = probability_dict[key]
+        if probability > 0:
+            entropy += -(probability*math.log(probability,2))
+    return entropy
     
 def generateStatistics(superGraph, cross_time_graphs, usrIdToUserDict, bnssIdToBusinessDict, reviewIdToReviewsDict):
     bnss_statistics = dict()
@@ -175,7 +183,19 @@ def generateStatistics(superGraph, cross_time_graphs, usrIdToUserDict, bnssIdToB
             bnss_statistics[bnssId][AVERAGE_RATING][timeKey] = float(sum(ratings))/float(len(ratings))
             
             #Rating Entropy
+            sorted_rating_list = set(sorted(ratings))
+            if RATING_DISTRIBUTION not in bnss_statistics[bnssId]:
+                bnss_statistics[bnssId][RATING_DISTRIBUTION] = dict()
+                
+            if timeKey not in bnss_statistics[bnssId][RATING_DISTRIBUTION]:
+                bnss_statistics[bnssId][RATING_DISTRIBUTION][timeKey] = {key:0.0 for key in sorted_rating_list}
             
+            for rating in ratings:
+                bnss_statistics[bnssId][RATING_DISTRIBUTION][timeKey][rating] += 1.0
+            
+            for rating in sorted_rating_list:
+                bnss_statistics[bnssId][RATING_DISTRIBUTION][timeKey][rating] /= float(len(reviews_for_bnss)) 
+              
             #NumberOfReviews
             if NO_OF_REVIEWS not in bnss_statistics[bnssId]: 
                 bnss_statistics[bnssId][NO_OF_REVIEWS] = numpy.zeros(total_time_slots)
@@ -228,52 +248,58 @@ def generateStatistics(superGraph, cross_time_graphs, usrIdToUserDict, bnssIdToB
                 youth_score = 1-sigmoid_prime((current_temporal_review_time-firstReviewDate).days)
                 youth_scores.append(youth_score)
             bnss_statistics[bnssId][YOUTH_SCORE][timeKey] = numpy.mean(numpy.array(youth_scores))
+            
             #Entropy Score
             
             #Max Text Similarity
     
     #POST PROCESSING FOR REVIEW AVERAGE_RATING AND RATING_ENTROPY
     for bnss_key in bnss_statistics:
-        #POST PROCESSING FOR AVERAGE RATING
         statistics_for_bnss = bnss_statistics[bnss_key]
         no_of_reviews_for_bnss = statistics_for_bnss[NO_OF_REVIEWS]
         avg_rating_for_bnss = statistics_for_bnss[AVERAGE_RATING]
         rating_sum_for_bnss = numpy.zeros(total_time_slots)
+        
         for timeKey in range(total_time_slots):
             rating_sum_for_bnss[timeKey] = no_of_reviews_for_bnss[timeKey]*avg_rating_for_bnss[timeKey]
          
         for timeKey in range(total_time_slots):
+            #POST PROCESSING FOR AVERAGE RATING
             if sum(no_of_reviews_for_bnss[:timeKey]) > 0:
                 statistics_for_bnss[AVERAGE_RATING][timeKey] = sum(rating_sum_for_bnss[:timeKey])/sum(no_of_reviews_for_bnss[:timeKey])
             else:
-                statistics_for_bnss[AVERAGE_RATING][timeKey] = 0 
+                statistics_for_bnss[AVERAGE_RATING][timeKey] = 0
+                
+            #POST PROCESSING FOR RATING ENTROPY        
+            if timeKey in statistics_for_bnss[RATING_DISTRIBUTION]:
+                entropy = entropyFn(statistics_for_bnss[RATING_DISTRIBUTION][timeKey])
+                if RATING_ENTROPY not in statistics_for_bnss:
+                    statistics_for_bnss[RATING_ENTROPY] = numpy.zeros(total_time_slots)
+                statistics_for_bnss[RATING_ENTROPY][timeKey] = entropy
 
     return bnss_statistics
 
-def plotBnssStatistics(bnss_statistics, bnssIdToBusinessDict, bnssKeys):
+
+def plotBnssStatistics(bnss_statistics, bnssIdToBusinessDict, bnss_key, clr):
+    bnss_name = bnssIdToBusinessDict[bnss_key].getName()
     for measure_key in MEASURES:
-        colors = itertools.cycle(['g', 'c', 'r', 'b', 'm', 'y', 'k'])
-        i=1
-        for bnss_key in bnssKeys:
-            clr = colors.next()
-            if measure_key not in bnss_statistics[bnss_key] or i>=5:
-                break
-            plt.title('Business Statistics') 
-            plt.xlabel('Time in multiples of 2 months')
-            plt.ylabel(measure_key)
-            plt.bar(range(len(bnss_statistics[bnss_key][measure_key])),\
-                     bnss_statistics[bnss_key][measure_key],\
-                     label=bnssIdToBusinessDict[bnss_key].getName(),
-                     color=clr, width=0.5, align="center")
-            i+=1
+        if measure_key not in bnss_statistics[bnss_key]:
+            continue
+        plt.title('Business statistics') 
+        plt.xlabel('Time in multiples of 2 months')
+        plt.ylabel(measure_key)
+        plt.bar(range(len(bnss_statistics[bnss_key][measure_key])),\
+                bnss_statistics[bnss_key][measure_key],\
+                label=bnssIdToBusinessDict[bnss_key].getName(),
+                color=clr, width=0.5, align="center")
         
-        if measure_key in bnss_statistics[bnss_key]:
-            art = []
-            lgd = plt.legend(loc=9, bbox_to_anchor=(0.5, -0.1), ncol=2)
-            art.append(lgd)
-            plt.savefig('/home/santhosh/'+measure_key+'.png', additional_artists=art,
-                             bbox_inches="tight")
-            plt.clf()
+        art = []
+        lgd = plt.legend(loc=9, bbox_to_anchor=(0.5, -0.1))
+        art.append(lgd)
+        plt.savefig('/home/santhosh/logs/'+bnss_name+'_'+measure_key+'.png',\
+                     additional_artists=art,\
+                     bbox_inches="tight")
+        plt.clf()
         
 
 if __name__ == '__main__':
@@ -296,4 +322,9 @@ if __name__ == '__main__':
     
     bnssKeys = [bnss_key for bnss_key in bnss_statistics]
     bnssKeys = sorted(bnssKeys, reverse=True, key = lambda x: len(superGraph.neighbors((x,SIAUtil.PRODUCT))))
-    plotBnssStatistics(bnss_statistics, bnssIdToBusinessDict, bnssKeys[:1])
+    
+    colors = ['g', 'c', 'r', 'b', 'm', 'y', 'k']
+    i=0
+    while i<5:
+        plotBnssStatistics(bnss_statistics, bnssIdToBusinessDict, bnssKeys[i], random.choice(colors))
+        i+=1
