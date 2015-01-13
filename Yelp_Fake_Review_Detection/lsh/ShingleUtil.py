7067f8c6679dc0b7b49a363e3817fb2b4136da1c
Santhosh Kumar Manavasi Lakshminarayanan <Santhosh Kumar Manavasi Lakshminarayanan>
2015-01-13 05:40:33 -0500

    Changes

--- Yelp_Fake_Review_Detection/lsh/ShingleUtil.py
+++ Yelp_Fake_Review_Detection/lsh/ShingleUtil.py
@@ -3,3 +3,49 @@ Created on Jan 12, 2015
 
 @author: santhosh
 '''
+import re
+import numpy
+import sys
+skip_words = '[\n]|[ ]|[\t]'
+
+def createAndReturnShingles(texts, n):
+    shingle_dict = dict()
+    text_index = 0
+    for text in texts:
+        shingle = ''
+        for t in text:
+            if t != ' ':
+                shingle = shingle+t
+            if(len(shingle) == n):
+                if shingle not in shingle_dict:
+                    shingle_dict[shingle] = numpy.zeros(len(texts))        
+                shingle_dict[shingle][text_index] = 1
+                shingle = ''
+    
+        if shingle != '':
+            if shingle not in shingle_dict:
+                    shingle_dict[shingle] = numpy.zeros(len(texts))
+            shingle_dict[shingle][text_index] = 1
+            
+        text_index+=1
+        
+    return shingle_dict
+
+def formDataMatrix(texts, n):
+    shingle_dict = createAndReturnShingles(texts, n)
+    
+    n = len(shingle_dict.keys())
+    m = len(texts)
+    
+    data_matrix = numpy.zeros(shape =(n,m))
+    shingle_list = []
+    
+    index = 0
+    for key in shingle_dict.keys():
+        data_matrix[index] = shingle_dict[key]
+        shingle_list.append(key)
+        index+=1
+
+    #print shingle_list, data_matrix
+    return data_matrix
+            
---
+++ Yelp_Fake_Review_Detection/lsh/__init__.py
@@ -0,0 +1,28 @@
+from util.ScrappedDataReader import ScrappedDataReader as sdr
+from util.GraphUtil import TemporalGraph
+from util import SIAUtil
+import ShingleUtil
+import sys
+import jac_doc_hash
+
+inputDirName = '/media/santhosh/Data/workspace/datalab/data/from ubuntu/main_zips'
+texts = ['abc def ghi jkl', 'bcd effg hijklm']
+data = ShingleUtil.formDataMatrix(texts, 5)
+print jac_doc_hash.jac_doc_hash(data, 10, 5)
+sys.exit()
+scr = sdr()
+(usrIdToUsrDict, bnssIdToBnssDict, reviewIdToReviewDict) = scr.readData(inputDirName)
+cross_time_graphs = TemporalGraph.createTemporalGraph(usrIdToUsrDict, bnssIdToBnssDict, reviewIdToReviewDict, '2-M')
+for time_key in cross_time_graphs:
+    G = cross_time_graphs[time_key]
+    for bnss_node in G.nodes():
+        bnss_key, bnss_type = bnss_node
+        if bnss_type == SIAUtil.PRODUCT:
+            reviewTextsInThisTimeBlock = [G.getReview(usr_key,bnss_key).getReviewText()\
+                                           for (usr_key, usr_type) in G.neighbors(bnss_node)]
+            if len(reviewTextsInThisTimeBlock) > 1: 
+                print time_key, bnss_key
+                data_matrix = ShingleUtil.formDataMatrix(reviewTextsInThisTimeBlock, 5)
+                jac_doc_hash.jac_doc_hash(data_matrix, 10, 5)
+                sys.exit()
+                print '------------------------------------------'
