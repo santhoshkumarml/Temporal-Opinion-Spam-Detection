3efd4f463adee0d881f926c70cce13fb87f38188
Santhosh Kumar Manavasi Lakshminarayanan <santhoshkumarml@gmail.com>
2015-04-20 20:29:34 -0400

    Changes

--- ReviewMining/tryout/testAlgos.py
+++ ReviewMining/tryout/testAlgos.py
@@ -3,10 +3,12 @@ __author__ = 'santhosh'
 import numpy
 import matplotlib.pyplot as plt
 
-CHPT1 = 'INCREASE_AND_DECREASE'
-CHPT2 = 'INCREASE_AND_INCREASE'
-OUTLIER1 = 'OUTLIER_INCREASE'
-OUTLIER2 = 'OUTLIER_DECREASE'
+CHPT_CONST_INCREASE = 'INCREASE_CONSTANT'
+CHPT_CONST_DECREASE = 'DECREASE_CONSTANT'
+CHPT_NORMAL_INCREASE = 'INCREASE_NORMAL'
+CHPT_NORMAL_DECREASE = 'DECREASE_NORMAL'
+OUTLIER_INCREASE = 'OUTLIER_INCREASE'
+OUTLIER_DECREASE = 'OUTLIER_DECREASE'
 
 def plotDataAndChanges(data, scores=[], changes=[]):
     fig = plt.figure()
@@ -22,6 +24,35 @@ def plotDataAndChanges(data, scores=[], changes=[]):
     plt.show()
 
 
-def makeNormalData(mean=0.7, varaince =0.05, data_size=200, induced_outlier_or_chpts=[], outlier_types = []):
+def makeNormalData(mean=0.7, varaince =0.05, data_size=200, induced_outlier_or_chpts=[], outlier_ch_types = []):
     data = numpy.random.normal(mean, varaince, data_size)
-    assert len(outlier_types) == len(induced_outlier_or_chpts)
+    outlier_ch_idxs_visited = set()
+    assert len(outlier_ch_types) == len(induced_outlier_or_chpts)
+    for idx in range(len(induced_outlier_or_chpts)):
+        induced_outlier_ch_idx, induced_outlier_ch_magnitude, persistence_time = induced_outlier_or_chpts[idx]
+        outlier_ch_type = outlier_ch_types[idx]
+        assert induced_outlier_ch_idx not in outlier_ch_idxs_visited
+        if outlier_ch_type == OUTLIER_INCREASE or outlier_ch_type == OUTLIER_DECREASE:
+            assert persistence_time == 1
+            if outlier_ch_type == OUTLIER_INCREASE:
+                data[induced_outlier_ch_idx] = mean+induced_outlier_ch_magnitude
+            else:
+                data[induced_outlier_ch_idx] = mean-induced_outlier_ch_magnitude
+        elif outlier_ch_type == CHPT_CONST_INCREASE:
+            start = induced_outlier_ch_idx
+            end = start+persistence_time
+            val = data[start-1] if start >=1 else mean
+            data[start:end] = val+induced_outlier_ch_magnitude
+        elif outlier_ch_type == CHPT_CONST_DECREASE:
+            start = induced_outlier_ch_idx
+            end = start+persistence_time
+            val = data[start-1] if start >=1 else mean
+            data[start:end] = val-induced_outlier_ch_magnitude
+        elif outlier_ch_type == CHPT_NORMAL_INCREASE:
+            start = induced_outlier_ch_idx
+            end = start+persistence_time
+            data[start:end] = numpy.random.normal(mean+induced_outlier_ch_magnitude, varaince, persistence_time)
+        elif outlier_ch_type == CHPT_NORMAL_DECREASE:
+            start = induced_outlier_ch_idx
+            end = start+persistence_time
+            data[start:end] = numpy.random.normal(mean-induced_outlier_ch_magnitude, varaince, persistence_time)
