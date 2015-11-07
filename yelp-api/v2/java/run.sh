39dbd0a64c2bcd45610a85aac0f7fbe2721ae06a
Santhosh Kumar Manavasi Lakshminarayanan <santhoshkumarml@gmail.com>
2015-11-07 16:13:12 -0500

    Changes

--- yelp-api/v2/java/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-EXAMPLE_CP=lib/scribe-1.3.5.jar:lib/json_simple-1.1.jar:lib/jcommander-1.35.jar:.
-
-mkdir -p build && javac -classpath $EXAMPLE_CP -d build YelpAPI.java && java -classpath $EXAMPLE_CP:build YelpAPI "$@"
