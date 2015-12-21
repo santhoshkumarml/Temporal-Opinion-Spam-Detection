3334b4b41189e0817f381fda9be219afc8a6f1a3
Santhosh Kumar Manavasi Lakshminarayanan <santhoshkumarml@gmail.com>
2015-12-21 00:22:36 -0500

    Refactoring

--- ReviewMining/text_utils/LDAUtil.py
+++ ReviewMining/text_utils/LDAUtil.py
@@ -13,17 +13,13 @@ tokenizer = nltk.tokenize.RegexpTokenizer(r'\w+')
 p_stemmer = nltk.stem.porter.PorterStemmer()
 
 def tokenizeReviewText(review_text):
-    raw = review_text.lower()
-    review_text_tokens = tokenizer.tokenize(raw)
-    return review_text_tokens
+    return tokenizer.tokenize(review_text.lower())
 
 def removeStopWordsForReview(review_text_tokens):
-    stopped_tokens = [i for i in review_text_tokens if not i in en_stop]
-    return stopped_tokens
+    return [token for token in review_text_tokens if not token in en_stop]
 
 def performStemmingforReview(review_text_stopped_tokens):
-    stemmed_tokens = [p_stemmer.stem(i) for i in review_text_stopped_tokens]
-    return stemmed_tokens
+    return [p_stemmer.stem(token) for token in review_text_stopped_tokens]
 
 def createDocumentWordMatrix(texts):
     dictionary = gensim.corpora.Dictionary(texts)
@@ -42,8 +38,8 @@ def performLDAOnReviews(reviews, num_topics=3, num_words=1):
     texts = []
     for review_text in review_texts:
         tokens = tokenizeReviewText(review_text)
-        tokens = removeStopWordsForReview(tokens)
-        tokens = performStemmingforReview(tokens)
-        texts.append(tokens)
+        stopped_tokens = removeStopWordsForReview(tokens)
+        stemmed_tokens = performStemmingforReview(stopped_tokens)
+        texts.append(stemmed_tokens)
     corpus, dictionary = createDocumentWordMatrix(texts)
     return performLDA(corpus, dictionary, num_topics=num_topics, num_words=num_words)
