5262d4c199968c48c18b9dbdd7b0356e76da55ec
Santhosh Kumar Manavasi Lakshminarayanan <santhoshkumarml@gmail.com>
2015-12-22 16:14:34 -0500

    Add all business to collect evidence flipkart

--- ReviewMining/main/testFlipKart.py
+++ ReviewMining/main/testFlipKart.py
@@ -16,11 +16,6 @@ if __name__ == "__main__":
     reader = FlipkartDataReader.FlipkartDataReader()
     currentDateTime = datetime.now().strftime('%d-%b--%H:%M')
     plotDir = os.path.join(os.path.join(os.path.join(csvFolder, os.pardir), 'stats'), 'fk')
-#     bnss_list = AppUtil.extractAndSerializeBnssStatisticsForBnss(csvFolder, plotDir,
-#                                                                  bnss_list_start=0,
-#                                                                  bnss_list_end=500000,
-#                                                                  rdr=reader)
-
 #     AppUtil.detectAnomaliesForBnsses(csvFolder, plotDir,
 #                                      StatConstants.MEASURE_CHANGE_THRES_FLIPKART,
 #                                      doPlot=True, dologStats=False,
@@ -34,8 +29,11 @@ if __name__ == "__main__":
 
 #     AppUtil.doRanking(plotDir)
 #     print ThresholdHelper.getThresholdForDifferentMeasures(plotDir, doHist=True)
-    bnss_key_time_wdw_list = [('5f2c7517a7012640763148a38b1372b6',(71, 76)),
-                              ('8df49a65474732e4f63d378df4bd67e4',(93, 98))]
+    bnss_key_time_wdw_list = [('8df49a65474732e4f63d378df4bd67e4', (93, 98)),
+                               ('5f2c7517a7012640763148a38b1372b6', (71, 76)),
+                                ('fb57b2749835facf54d9c73f0d9a8d4c', (32, 37)),
+                                 ('8edd789d64c7279592057487ff5bb264', (31, 36)),
+                                  ('a9856cb97ebd363a0581d08f27f8b379', (30, 35))]
     AppUtil.doGatherEvidence(csvFolder, plotDir,
                              rdr=reader,
                              bnss_key_time_wdw_list=bnss_key_time_wdw_list)
