ba2a53015c7ddb8a5188320e7ceafad8a6602177
Santhosh Kumar Manavasi Lakshminarayanan <santhoshkumarml@gmail.com>
2016-01-06 13:36:48 +0530

    Removing unwanted files

--- Granger_source/GrangerAD/Parallel_L1_Minimization/make.m
+++
@@ -1,5 +0,0 @@
-addpath('SCD');
-mex 'mex_PStocSubGD.cpp'
-mex 'mex_PSGD_Yahoo.cpp'
-mex 'mex_SGLD.cpp'
-mex 'SCD/mex_SCD.cpp'
--- Granger_source/GrangerAD/Parallel_L1_Minimization/readme.txt
+++
@@ -1,4 +0,0 @@
-Compile the cpp file by executing make.m
-The main function is lasso_granger.m
-See examples in test.m
-
